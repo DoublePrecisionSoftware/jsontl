084c67ac31d27f026b686d4eeec8866f54a238f7
Stephen Collins <rr154459@GA8995AC511228.local>
2015-03-23 12:57:43 -0400

    fixed formatting and various lint issues

--- jsontl.js
+++ jsontl.js
@@ -1,139 +1,134 @@
 (function () {
 	'use strict';
 
-	var jsontl = (function () {
+	var transforms = (function () {
 
-		var transforms = (function () {
+		function getMatch(node, trans) {
+			function nodeMatch(e) {
+				return node[e] === match[e];
+			}
 
-			function getMatch(node, trans) {
-				function nodeMatch(e) {
-					return node[e] === match[e];
-				}
-
-				for (var prop in trans) {
-					var newVal = trans[prop].with;
-					var match = trans[prop].when;
-					if (node.hasOwnProperty(prop)) {
-						if (typeof match === 'object') {
-							var matched = Object.keys(match).every(nodeMatch);
-							if (matched) {
-								node[prop] = newVal;
-							}
-						} else if (!match || (node[prop] == match)) {
+			for (var prop in trans) {
+				var newVal = trans[prop].with;
+				var match = trans[prop].when;
+				if (node.hasOwnProperty(prop)) {
+					if (typeof match === 'object') {
+						var matched = Object.keys(match).every(nodeMatch);
+						if (matched) {
 							node[prop] = newVal;
 						}
+					} else if (!match || (node[prop] == match)) {
+						node[prop] = newVal;
 					}
 				}
 			}
+		}
 
-			function replace(node, trans, key) {
-				if (node[key] instanceof Array) {
-					var i = -1,
-						len = node[key].length;
-					while (++i < len) {
-						getMatch(node[key][i], trans);
-					}
-				} else if (typeof node[key] === 'object') {
-					getMatch(node[key], trans);
-				} else {
-					var newVal = trans.with;
-					var match = trans.when;
-					if (!match || (node[key] == match)) {
-						node[key] = newVal;
-					}
+		function replace(node, trans, key) {
+			if (node[key] instanceof Array) {
+				var i = -1,
+					len = node[key].length;
+				while (++i < len) {
+					getMatch(node[key][i], trans);
+				}
+			} else if (typeof node[key] === 'object') {
+				getMatch(node[key], trans);
+			} else {
+				var newVal = trans.with;
+				var match = trans.when;
+				if (!match || (node[key] == match)) {
+					node[key] = newVal;
 				}
 			}
+		}
 
-			// add the specified data to the object provided
-			var extend = function (data, trans, key) {
-				if (typeof data[key] !== "object") {
-					throw new TypeError("Cannot extend a scalar value.");
-				}
-
-				for (var prop in trans) {
-					if (typeof trans[prop] === "object") {
-						return extend(data[key], trans[prop], prop);
-					}
-					data[key][prop] = trans[prop];
-				}
+		// add the specified data to the object provided
+		var extend = function (data, trans, key) {
+			if (typeof data[key] !== "object") {
+				throw new TypeError("Cannot extend a scalar value.");
 			}
 
-			var push = function(node, trans, key) {
-				if (!node[key] instanceof Array){
-					throw TypeError('The push operation is only valid on Array types.');
+			for (var prop in trans) {
+				if (typeof trans[prop] === "object") {
+					return extend(data[key], trans[prop], prop);
 				}
-				if (!trans instanceof Array) {
-					throw TypeError('The "push" operator requires an Array value.');
-				}
-
-				trans.forEach(function(val) {
-					node[key].push(val);
-				});
-			};
-
-			return {
-				replace: replace,
-				extend: extend,
-				push: push
-			};
-		})();
-
-		var locators = (function () {
-
-			var _in = function (data, transform) {
-				// look at the transform object for member to transform
-				for (var definition in transform) {
-					// each tranform definition contains an array of transform instruction objects
-					transform[definition].forEach(function (def) {
-						// perform each of the transform operations defined in `def`
-						for (var op in def) {
-							// ensure the operation or locator exists
-							if (transforms[op]) {
-								// perform the transform
-								if (data instanceof Array) {
-									data.forEach(function (d) {
-										transforms[op](d, def[op], definition);
-									});
-								} else {
-									transforms[op](data, def[op], definition);
-								}
-							} else if (locators[op]) {
-								// navigate to each location
-								return locators[op](data[definition], def[op]);
-							}
-							// otherwise, move on
-						}
-					});
-				}
-				return data;
-			}
-
-			return {
-				in : _in
-			};
-		})();
-
-
-		var lib = {
-
-			/**
-			 * Perform the provided transform on the data
-			 * @param   {Object} data  Data to transform
-			 * @param   {Object} trans Transform definition
-			 * @returns {Object} The transformed data
-			 */
-			transform: function (data, transform) {
-				if (typeof transform.jsontl !== "undefined") {
-					if (typeof transform.jsontl.transform !== "undefined") {
-						return locators.in(data, transform.jsontl.transform);
-					}
-				}
-				throw Error("Tranform definition invalid");
+				data[key][prop] = trans[prop];
 			}
 		};
 
-		return lib;
+		var push = function (node, trans, key) {
+			if (!node[key] instanceof Array) {
+				throw new TypeError('The push operation is only valid on Array types.');
+			}
+			if (!trans instanceof Array) {
+				throw new TypeError('The "push" operator requires an Array value.');
+			}
+
+			trans.forEach(function (val) {
+				node[key].push(val);
+			});
+		};
+
+		return {
+			replace: replace,
+			extend: extend,
+			push: push
+		};
 	})();
 
-	module.exports = jsontl;
+	var locators = (function () {
+
+		var _in = function (data, transform) {
+			// look at the transform object for member to transform
+			for (var definition in transform) {
+				// each tranform definition contains an array of transform instruction objects
+				transform[definition].forEach(function (def) {
+					// perform each of the transform operations defined in `def`
+					for (var op in def) {
+						// ensure the operation or locator exists
+						if (transforms[op]) {
+							// perform the transform
+							if (data instanceof Array) {
+								data.forEach(function (d) {
+									transforms[op](d, def[op], definition);
+								});
+							} else {
+								transforms[op](data, def[op], definition);
+							}
+						} else if (locators[op]) {
+							// navigate to each location
+							return locators[op](data[definition], def[op]);
+						}
+						// otherwise, move on
+					}
+				});
+			}
+			return data;
+		};
+
+		return {
+			in : _in
+		};
+	})();
+
+
+	var lib = {
+
+		/**
+		 * Perform the provided transform on the data
+		 * @param   {Object} data  Data to transform
+		 * @param   {Object} trans Transform definition
+		 * @returns {Object} The transformed data
+		 */
+		transform: function (data, transform) {
+			if (typeof transform.jsontl !== "undefined") {
+				if (typeof transform.jsontl.transform !== "undefined") {
+					return locators.in(data, transform.jsontl.transform);
+				}
+			}
+			throw new Error("Tranform definition invalid");
+		}
+	};
+
+	module.exports = lib;
 })();
