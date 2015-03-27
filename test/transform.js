5c31061c98f6ca123bf4f7a0bf84dc46bf8c75c5
Stephen Collins <rr154459@GA8995AC511228.local>
2015-03-26 23:48:43 -0400

    fixed formatting

--- test/transform.js
+++ test/transform.js
@@ -6,87 +6,87 @@ module.exports = {
 				{
 					"replace": {
 						"with": "Two"
+					}
 				}
-      }
-     ],
-		"Person": [
-			{
-				"replace": {
-					"FirstName": {
-						"with": "Bob",
-						"when": {
-							"LastName": "Smith"
+			],
+			"Person": [
+				{
+					"replace": {
+						"FirstName": {
+							"with": "Bob",
+							"when": {
+								"LastName": "Smith"
+							}
+						},
+						"LastName": {
+							"with": "Jones"
 						}
 					},
-					"LastName": {
-						"with": "Jones"
-					}
-				},
-				"extend": {
-					"MiddleInitial": "X"
-				}
-    	}
-   	],
-		"System": [
-		{
-			"in": {
-				"Meta": [
-					{
-						"replace": {
-							"OS": {
-								"with": "Windows"
-							}
-						},
-						"extend": {
-							"User": "Bob"
-						}
-					}
-				]
-			}
-    }, {
-			"replace": {
-				"Name": {
-					"with": "MySytem2"
-				}
-			}
-    }
-   ],
-	"Customers": [
-		{
-			"replace": {
-				"Name": {
-					"with": "Jerry",
-					"when": {
-						"Id": 1
+					"extend": {
+						"MiddleInitial": "X"
 					}
 				}
-			}
-		},
-		{
-			"in": {
-				"Addresses": [
-					{
-						"replace": {
-							"Address1": {
-								"with": "321 Code Street",
-								"when": {
-									"Type": "Work"
+			],
+			"System": [
+				{
+					"in": {
+						"Meta": [
+							{
+								"replace": {
+									"OS": {
+										"with": "Windows"
+									}
+								},
+								"extend": {
+									"User": "Bob"
 								}
 							}
-						},
-						"push": [
+						]
+					}
+				}, {
+					"replace": {
+						"Name": {
+							"with": "MySytem2"
+						}
+					}
+				}
+			],
+			"Customers": [
+				{
+					"replace": {
+						"Name": {
+							"with": "Jerry",
+							"when": {
+								"Id": 1
+							}
+						}
+					}
+				},
+				{
+					"in": {
+						"Addresses": [
 							{
-								"Type": "Vacation",
-								"Address1": "123 Bahamas Ave",
-								"City": "The",
-								"State": "Bahamas"
+								"replace": {
+									"Address1": {
+										"with": "321 Code Street",
+										"when": {
+											"Type": "Work"
+										}
+									}
+								},
+								"push": [
+									{
+										"Type": "Vacation",
+										"Address1": "123 Bahamas Ave",
+										"City": "The",
+										"State": "Bahamas"
+									}
+								]
 							}
 						]
-	 				}
-				]
-			}
-		}
-	]
+					}
+				}
+			]
 		}
 	}
 };
